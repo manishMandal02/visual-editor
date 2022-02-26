802d3db4df2c7507a4dd75fdfc4b56261793c945
manishMandal02 <manishmandalj@gmail.com>
2022-02-27 04:08:15 +0530

    minor fix

--- src/utils/resetSelectEl.ts
+++
@@ -1,9 +0,0 @@
-import { useCallback } from 'react'
-import { useCircleStore } from '../store/shapes'
-
-const resetSelectEl = () => {
-  const selectCircle = useCircleStore((state) => state.select)
-  selectCircle(null)
-}
-
-export default resetSelectEl
